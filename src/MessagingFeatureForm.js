0c8c7c51f0b623d4645fd7542d7fdb2ef8735107
yahya dalbah <me@yahyadalbah.com>
2023-12-11 19:19:45 +0200

    made user able to type emojis

--- src/MessagingFeatureForm.js
+++ src/MessagingFeatureForm.js
@@ -1,9 +1,12 @@
 import { child, get, ref, set } from "firebase/database";
 import React, { useState } from "react";
 import { db } from "./config";
+import EmojiPicker from "emoji-picker-react";
+import "./messagingForm.css"
 
-export default function MessagingFeatureForm({ userData, chatId = 1 }) {
+export default function MessagingFeatureForm({ userData, chatId = 0 }) {
   const [message, setMessage] = useState("");
+  const [showPicker,setShowPicker] = useState(false)
   function sendMessage(e) {  //maybe change the function after we progress the project 
     const dbRef = ref(db);
     e.preventDefault();
@@ -16,23 +19,46 @@ export default function MessagingFeatureForm({ userData, chatId = 1 }) {
         setMessage("");
     })
   }
+  function onEmojiClick(emojiObject,e){
+    e.preventDefault()
+    setMessage(prev => prev + emojiObject.emoji)
+    setShowPicker(false)
+  }
   return (
-    <form action="">
-      <input
-        placeholder="Type a message"
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button>
-        <i class="fa-solid fa-face-smile"></i>
-      </button>
-      <button>
-        <i class="fa-solid fa-paperclip"></i>
-      </button>
-      <button onClick={(e) => {sendMessage(e)}}>
-        <i class="fa-solid fa-paper-plane"></i>
-      </button>
-    </form>
+    <div className="d-flex flex-column">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="d-flex gap-2 m-2 mt-5 "
+        action=""
+      >
+        <input
+          placeholder="Type a message"
+          class="form-control"
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <div>
+          <button class="btn btn-danger" onClick={() => setShowPicker(prev => !prev)}>
+            <i class="fa-solid fa-face-smile"></i>
+          </button>
+        </div>
+
+        <button className="btn btn-secondary">
+          <i class="fa-solid fa-paperclip"></i>
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={(e) => {
+            if (message) {
+              sendMessage(e);
+            }
+          }}
+        >
+          <i class="fa-solid fa-paper-plane"></i>
+        </button>
+      </form>
+      {showPicker && <EmojiPicker width={"100%"} height={"400px"} onEmojiClick={onEmojiClick} />}
+    </div>
   );
 }
