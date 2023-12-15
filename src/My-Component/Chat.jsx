491b09e1d0f05ad013f7b451e8427e7e9f457fe8
MarselHanani <s12220672@stu.najah.edu>
2023-12-15 18:47:06 +0200

    add delete messages function and make some edit on my component

--- src/My-Component/Chat.jsx
+++ src/My-Component/Chat.jsx
@@ -1,15 +1,6 @@
 import {Messages} from "./Messages";
-import {useEffect, useState} from "react";
-import {onValue, ref} from "firebase/database";
-import {db} from "../config";
-export function Chat({chat_id = 0, userData}) {
-    const [chats, setChats] = useState([]);
-    useEffect(() => {
-        onValue(ref(db, `chats/${chat_id}/messages`), (snapshot) => {
-            const data = snapshot.val();
-            setChats(data);
-        })
-    },[]);
+export function Chat({userData , chats}) {
+
     return (
         <div className='bg-light h-78 border-1 border-bottom border-secondary-light'>
             <Messages chats={chats} userData={userData}/>
--- src/My-Component/ChatMessages.jsx
+++ src/My-Component/ChatMessages.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Chat} from "./Chat";
 import {HeaderChat} from "./HeaderChat";
 import MessagingFeatureForm from "../MessagingFeatureForm";
-export function ChatMessages({userData}){
+import {onValue, ref} from "firebase/database";
+import {db} from "../config";
+export function ChatMessages({chat_id = 0,userData}){
+    const [chats, setChats] = useState([]);
+    useEffect(() => {
+        onValue(ref(db, `chats/${chat_id}/messages`), (snapshot) => {
+            const data = snapshot.val();
+            if(data) setChats(data);
+        })
+    },[]);
     return(
         <div className='chatMessages bg-light '>
-            <HeaderChat userData={userData} />
-            <Chat userData={userData}/>
+            <HeaderChat userData={userData} chat_id={chat_id} chats={chats}/>
+            <Chat userData={userData} chat_id={chat_id} chats={chats}/>
             <MessagingFeatureForm userData={userData} />
         </div>
     )
