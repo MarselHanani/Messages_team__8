b86553e380a122a995fb546d404296cc1824fb33
yahya dalbah <me@yahyadalbah.com>
2023-12-15 16:31:15 +0200

    added message if there's no messages

--- src/My-Component/Messages.jsx
+++ src/My-Component/Messages.jsx
@@ -3,11 +3,11 @@ import { Message } from "./Message";
 export default forwardRef(function Messages({ chats, userData },ref) {
   return (
     <div className="messages d-flex flex-column">
-      {chats.map((value) => {
+      {chats ? chats.map((value) => {
         return (
           <Message message={value} userData={userData} />
         );
-      })}
+      }) : <h2 style={{textAlign: "center"}}>you have no messages with this user <br></br> say Hi!</h2>}
       <div ref={ref}></div>
     </div>
   );
