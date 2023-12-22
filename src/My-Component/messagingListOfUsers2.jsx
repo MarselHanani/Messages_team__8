550ae282af6bbdea3929752c146edb320ee79a38
omar <oabuhilal11@gmail.com>
2023-12-22 18:22:57 +0200

    edit 2 file

--- src/My-Component/messagingListOfUsers2.jsx
+++ src/My-Component/messagingListOfUsers2.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Search from "../Search";
 import "./ListOfUsers.css"
 import './listOfUser2.css';
@@ -8,26 +9,19 @@ export default function MessagingListOfUsers( {
 })
 
 {
-  return (
-    <div className="sidebar bg-light pt-5">
-      <h3>
-        Active Chats{"  "}
-        <span style={{ color: "black", backgroundColor: "#198754" }}>
-          {usersChats.length}
-        </span>
-        
-      </h3>
-      <Search {...{ usersChats, setDisplayedChat }} />
-
-      <div className="users-list">
-        {usersChats.map((chat, index) => (
-          <div key={index} className="user-item">
-            <img src={chat.userImage} className="user-img" alt="User" />
-            <span className="user-name">{chat.userName}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
   
+return (
+  <div className="sidebar bg-light pt-5">
+    <h3>Active Chats <span style={{ color: "black", backgroundColor: "#198754" }}>{usersChats.length}</span></h3>
+    <Search usersChats={usersChats} setDisplayedChat={setDisplayedChat} />
+    <div className="users-list">
+      {usersChats.map((chat, index) => (
+        <div key={index} className="user-item" onClick={() => setDisplayedChat(chat)}>
+          {chat.userImage && <img src={chat.userImage} className="user-img" alt={`${chat.userName}`} />}
+          <span className="user-name">{chat.userName}</span>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 }
