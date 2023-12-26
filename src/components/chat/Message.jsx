1e8c14f1ed69a3e978631ce54a185a07d584ac9c
bissan <s12218588@stu.najah.edu>
2023-12-26 20:52:18 +0200

    date task

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-
+import { TimeMessage } from "./MessageTime";
 export function Message({ message, userData, otherUser }) {
+  const x=message.time;
+  const styles = {
+    display: "flex",
+    justifyContent: "right",
+    alignItems: "right",
+  };
   function ownerMessage() {
     return (
+      <>
       <div className="d-flex gap-3 flex-row-reverse m-2 ">
         <div className=" ms-2 mt-2-5 d-flex flex-column">
           <img
@@ -16,10 +23,21 @@ export function Message({ message, userData, otherUser }) {
           <p className="mt-1 message-owner mx-mx-content">{message.message}</p>
         </div>
       </div>
+      <div style={styles}>
+      <TimeMessage tr={x}/>
+
+      </div>
+      </>
     );
   }
   function otherMessage() {
+    const styles2 = {
+      display: "flex",
+      justifyContent: "left",
+      alignItems: "left",
+    };
     return (
+      <>
       <div className="d-flex gap-3 m-2">
         <div className=" ms-2 mt-1 d-flex flex-column align-items-start ">
           <img
@@ -33,6 +51,11 @@ export function Message({ message, userData, otherUser }) {
           <p className="mt-1 message-other mx-mx-content">{message.message}</p>
         </div>
       </div>
+           <div style={styles2}>
+           <TimeMessage tr={x}/>
+     
+           </div>
+           </>
     );
   }
   return (
--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -1,21 +1,50 @@
 import { forwardRef } from "react";
 import { Message } from "./Message";
+import { DateMessage } from "./DateMessage";
 export default forwardRef(function Messages(
   { chats, userData, otherUser },
   ref
 ) {
+let groups;
+if(chats)
+{   groups = chats.reduce((groups, chat) => {
+   let chatDate=new Date(chat.time);
+    const date = chatDate.toDateString();
+    
+    if (!groups[date]) {
+      groups[date] = [];
+    }
+    groups[date].push(chat);
+    return groups;
+  }, {});
+}
   return (
     <div className="messages d-flex flex-column">
       {chats ? (
-        chats.map((value) => {
-          return (
-            <Message
-              message={value}
-              userData={userData}
-              otherUser={otherUser}
-            />
-          );
-        })
+    Object.entries(groups).map(([key,messages])=>{
+      return(
+     <div>
+        <DateMessage date={key} />
+ {  <div>
+      {
+      messages.map((value) => {
+      
+        return (
+     
+         
+          <Message
+            message={value}
+            userData={userData}
+            otherUser={otherUser}
+          />
+       
+        );
+      })
+    }</div>}
+    </div>
+      )
+    })
+ 
       ) : (
         <h2 style={{ textAlign: "center" }}>
           you have no messages with this user <br></br> say Hi!
